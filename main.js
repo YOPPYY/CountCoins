56fe0748d39c76e95a76b1e6b357632c71a8fec8
TEIKOU <yoshiki444.jp@gmail.com>
2021-02-16 17:08:09 +0900

    0216

--- main.js
+++ main.js
@@ -9,25 +9,35 @@ var PIECE_SIZE = GRID_SIZE * 0.95; // ピースの大きさ
 var PIECE_NUM_XY = 4;              // 縦横のピース数
 var PIECE_OFFSET = GRID_SIZE / 2;  // オフセット値
 
-var group;
 var x=[];
 var y=[];
+
 var coins =["coin1","coin5","coin10","coin50","coin100","coin500"];
 var price =[1,5,10,50,100,500];
+
 var ans=0;
 var time=0;
-var now=0;
+
 var countdown;
+
 var score=0;
+var input = 0;
+
+var level = 5;
+var speed = 1;
+
+var clear=0;
 
 var ASSETS = {
   image: {
-    'coin1': 'coin/money_coin_heisei_1.png',
-    'coin5': 'coin/money_coin_blank_5.png',
-    'coin10': 'coin/money_coin_heisei_10.png',
-    'coin50': 'coin/money_coin_heisei_50.png',
-    'coin100': 'coin/money_coin_heisei_100.png',
-    'coin500': 'coin/money_coin_reiwa_500_new.png',
+    'coin1': 'img/money_coin_heisei_1.png',
+    'coin5': 'img/money_coin_blank_5.png',
+    'coin10': 'img/money_coin_heisei_10.png',
+    'coin50': 'img/money_coin_heisei_50.png',
+    'coin100': 'img/money_coin_heisei_100.png',
+    'coin500': 'img/money_coin_reiwa_500_new.png',
+    'maru': 'img/mark_maru.png',
+    'batsu': 'img/mark_batsu.png',
   },
 };
 
@@ -39,18 +49,18 @@ phina.define('Title', {
     // 背景色を指定
     this.backgroundColor = '#444';
     // ラベルを生成
-    this.label = Label('Count Coins').addChildTo(this);
+    this.label = Label({text:'Count Coins', fontSize: 48}).addChildTo(this);
     this.label.x = this.gridX.center(); // x 座標
     this.label.y = this.gridY.center()-50; // y 座標
     this.label.fill = 'white'; // 塗りつぶし色
-    this.label.setScale(1.5);
 
     this.label = Label('Tap to Start').addChildTo(this);
     this.label.x = this.gridX.center(); // x 座標
     this.label.y = this.gridY.center()+50; // y 座標
     this.label.fill = 'white'; // 塗りつぶし色
 
-
+    score=0;
+    clear=0;
 
     for(let i in coins){
       var coin = Sprite(coins[i])
@@ -89,9 +99,10 @@ phina.define('Main', {
     this.label.y = this.gridY.center()-50; // y 座標
     this.label.fill = 'white'; // 塗りつぶし色
 
-    sum=6;
-    time = 20 * sum;
-    console.log("sum:"+sum +" time:"+ time+" now:"+now);
+    ans = 0;
+    input =0;
+    time = 100 + (level-5)*7.5;
+    console.log(time/20);
 
     var gauge = Gauge({
       x: SCREEN_WIDTH/2, y: SCREEN_HEIGHT-15,        // x,y座標
@@ -115,28 +126,8 @@ phina.define('Main', {
       self.exit();
     };
 
-    /*
-    var angle = Math.floor( Math.random() * 360 );
-    var x = 10 * Math.cos( angle * (Math.PI / 180) ) ;
-    var y = 10 * Math.sin( angle * (Math.PI / 180) ) ;
-
-    var coin = Sprite('coin10')
-    .addChildTo(this)
-    .setPosition(this.gridX.center(), this.gridY.center())
-    .setScale(0.2);
-
-
-    var coin2 = Sprite('coin100')
-    .addChildTo(this)
-    .setPosition(SCREEN_WIDTH-coin.width*0.1, SCREEN_HEIGHT-coin.height*0.1)
-    .setScale(0.2);
-
-    console.log(coin2.height +" "+coin2.width);
-    console.log(coin2.x +" "+coin2.y);
-    */
-    ans=0;
-    group = DisplayElement().addChildTo(this);
-    for(n=0;n<sum;n++){
+    var group = DisplayElement().addChildTo(this);
+    for(n=0;n<level;n++){
       var r = Math.floor( Math.random() * coins.length );
       var angle = Math.floor( Math.random() * 360 );
       var sprite = Sprite(coins[r]).addChildTo(group)
@@ -156,7 +147,7 @@ phina.define('Main', {
 
       group.update= function(){
         for(i=0;i<group.children.length;i++){
-          group.children[i].moveBy(x[i], y[i]);
+          group.children[i].moveBy(x[i]*0.8, y[i]*0.8);
 
 
           //console.log(coin.x +", "+coin.y);
@@ -186,135 +177,265 @@ phina.define('Main', {
       // 親クラス初期化
       this.superInit();
       var self=this;
-      var button=[];
+
       // 背景色を指定
       this.backgroundColor = '#444';
       // ラベルを生成
       this.label = Label('答えを入力').addChildTo(this);
       this.label.x = this.gridX.center(); // x 座標
-      this.label.y = this.gridY.center()/4; // y 座標
+      this.label.y = 75; // y 座標
       this.label.fill = 'white'; // 塗りつぶし色
+
+
+      var label = Label({x:320,y:150,fontSize:64,fill:'white',text:input}).addChildTo(this);
+
 
       var gauge2 = Gauge({
         x: SCREEN_WIDTH/2, y: SCREEN_HEIGHT-15,        // x,y座標
         width: SCREEN_WIDTH,            // 横サイズ
         height: 30,            // 縦サイズ
         cornerRadius: 0,      // 角丸み
-        maxValue: 500,         // ゲージ最大値
-        value: 500,         // ゲージ初期値
+        maxValue: 150,         // ゲージ最大値
+        value: 150,         // ゲージ初期値
         fill: 'white',         // 後ろの色
         gaugeColor: 'skyblue', // ゲージ色
         stroke: 'silver',      // 枠色
         strokeWidth: 5,        // 枠太さ
       }).addChildTo(this);
 
+      var piecegroup = DisplayElement().addChildTo(this);
 
-      var number=['1','2','3','4','5','6','7','8','9'];
-      var n=0;
-      for(var i=0;i<3;i++){
-        for(var j=0;j<3;j++){
-            button[n] = Button({
-            x: 320+175*j-175,             // x座標
-            y: 480-175*i+175,             // y座標
-            width: 150,         // 横サイズ
-            height: 150,        // 縦サイズ
-            text: number[n],     // 表示文字
-            fontSize: 32,       // 文字サイズ
-            fontColor: 'white', // 文字色
-            cornerRadius: 10,   // 角丸み
-            fill: 'skyblue',    // ボタン色
-            stroke: 'blue',     // 枠色
-            strokeWidth: 5,     // 枠太さ
-            // 他にも指定できる…？
-          }).addChildTo(this);
-          n++;
-        }
+      //ボタンOK
+      var button = Button({
+        x: 320+175,             // x座標
+        y: 480+350,             // y座標
+        width: 150,         // 横サイズ
+        height: 150,        // 縦サイズ
+        text: "OK",     // 表示文字
+        fontSize: 48,       // 文字サイズ
+        fontColor: 'red', // 文字色
+        cornerRadius: 10,   // 角丸み
+        fill: "lightsalmon",    // ボタン色
+        stroke: 'red',     // 枠色
+        strokeWidth: 5,     // 枠太さ
+        // 他にも指定できる…？
+      }).addChildTo(piecegroup)
+      .onpointstart = function() {
+
+        clearInterval(countdown);
+        self.exit("result");
+
+      };
+
+      //ボタンBACK
+      var button = Button({
+        x: 320-175,             // x座標
+        y: 480+350,             // y座標
+        width: 150,         // 横サイズ
+        height: 150,        // 縦サイズ
+        text: "←",     // 表示文字
+        fontSize: 48,       // 文字サイズ
+        fontColor: 'black', // 文字色
+        cornerRadius: 10,   // 角丸み
+        fill: 'lightgray',    // ボタン色
+        stroke: 'black',     // 枠色
+        strokeWidth: 5,     // 枠太さ
+        // 他にも指定できる…？
+      }).addChildTo(piecegroup)
+      .onpointstart = function() {
+
+        if(input < 10){input = 0;}
+        else{input = Math.floor(input * 0.1);}
+        label.text = input;
+
+        console.log(this.text);
+      };
+
+      //ボタン0
+      var button = Button({
+        x: 320,             // x座標
+        y: 480+350,             // y座標
+        width: 150,         // 横サイズ
+        height: 150,        // 縦サイズ
+        text: 0,     // 表示文字
+        fontSize: 48,       // 文字サイズ
+        fontColor: 'blue', // 文字色
+        cornerRadius: 10,   // 角丸み
+        fill: 'skyblue',    // ボタン色
+        stroke: 'blue',     // 枠色
+        strokeWidth: 5,     // 枠太さ
+        // 他にも指定できる…？
+      }).addChildTo(piecegroup)
+      .onpointstart = function() {
+
+        if(input==0){input=this.text;}
+        else{input = input * 10 + this.text;}
+        label.text = input;
+
+        console.log(this.text);
+      };
+
+      //ボタン1-9
+      for(var n=0;n<9;n++){
+        var i= Math.floor(n/3);
+        var j= n%3;
+        var button = Button({
+          x: 320+175*j-175,             // x座標
+          y: 480-175*i+175,             // y座標
+          width: 150,         // 横サイズ
+          height: 150,        // 縦サイズ
+          text: n+1,     // 表示文字
+          fontSize: 48,       // 文字サイズ
+          fontColor: 'blue', // 文字色
+          cornerRadius: 10,   // 角丸み
+          fill: 'skyblue',    // ボタン色
+          stroke: 'blue',     // 枠色
+          strokeWidth: 5,     // 枠太さ
+          // 他にも指定できる…？
+        }).addChildTo(piecegroup)
+        .onpointstart = function() {
+
+          if(input==0){input=this.text;}
+          else{input = input * 10 + this.text;}
+          label.text = input;
+
+          console.log(this.text);
+        };
       }
-      button.onpointstart=function(){
-        console.log("push");
-      }
+
 
       countdown = function(){
         gauge2.value --;
       };
       setInterval(countdown, 50);
+
       gauge2.onempty = function(){
         clearInterval(countdown);
-        self.exit();
+        self.exit("result");
       };
 
 
 
     },
 
-/*
+    /*
     onpointstart: function() {
-      this.exit();
-    },
-*/
+    this.exit();
+  },
+  */
+});
+
+// Result クラスを定義
+phina.define('Result', {
+  superClass: 'CanvasScene',
+  init: function() {
+    // 親クラス初期化
+    this.superInit();
+    var self=this;
+    var next="";
+    var result="";
+    var result2="";
+    var text="";
+
+    if(ans==input){
+      result="Tap to Next Level";
+      text ="次へ";
+      level++;
+      clear++;
+      next = "main";
+      score += ans;
+      var mark="maru";
+    }
+
+    if(ans!=input){
+      result="ゲームオーバー";
+      result2="スコア："+score;
+      text= "タイトルへ";
+      next = "title";
+      var mark="batsu";
+    }
+
+    // 背景色を指定
+    this.backgroundColor = '#444';
+    // ラベルを生成
+    this.label = Label({text:'正解:'+ans+"円",fontSize:48}).addChildTo(this);
+    this.label.x = this.gridX.center(); // x 座標
+    this.label.y = this.gridY.center()-48; // y 座標
+    this.label.fill = 'white'; // 塗りつぶし色
+
+
+    this.label = Label({text:result,fontSize:48}).addChildTo(this);
+    this.label.x = this.gridX.center(); // x 座標
+    this.label.y = this.gridY.center()+48; // y 座標
+    this.label.fill = 'white'; // 塗りつぶし色
+
+    this.label = Label({text:result2,fontSize:32}).addChildTo(this);
+    this.label.x = this.gridX.center(); // x 座標
+    this.label.y = this.gridY.center()+96; // y 座標
+    this.label.fill = 'white'; // 塗りつぶし色
+
+    var sprite = Sprite(mark).addChildTo(this)
+    .setPosition(SCREEN_WIDTH/2,240).setScale(0.5);
+
+    var button = Button({
+      x: 320,             // x座標
+      y: 480+240,             // y座標
+      width: 300,         // 横サイズ
+      height: 150,        // 縦サイズ
+      text: text,     // 表示文字
+      fontSize: 48,       // 文字サイズ
+      fontColor: 'blue', // 文字色
+      cornerRadius: 10,   // 角丸み
+      fill: 'skyblue',    // ボタン色
+      stroke: 'blue',     // 枠色
+      strokeWidth: 5,     // 枠太さ
+      // 他にも指定できる…？
+    }).addChildTo(this)
+    .onpointstart = function() {
+
+      self.exit(next);
+    };
+
+  },
+
+
+
+});
+
+
+// メイン処理
+phina.main(function() {
+
+  // アプリケーションを生成
+  var app = GameApp({
+    query: '#canvas',
+    // Scene01 から開始
+    startLabel: 'title',
+    assets: ASSETS,
+    // シーンのリストを引数で渡す
+    scenes: [
+      {
+        className: 'Title',
+        label: 'title',
+        nextLabel: 'main',
+      },
+
+      {
+        className: 'Main',
+        label: 'main',
+        nextLabel: 'answer',
+      },
+      {
+        className: 'Answer',
+        label: 'answer',
+        nextLabel: 'result',
+      },
+      {
+        className: 'Result',
+        label: 'result',
+      },
+    ]
   });
-
-  // Result クラスを定義
-  phina.define('Result', {
-    superClass: 'CanvasScene',
-    init: function(message) {
-      // 親クラス初期化
-      this.superInit();
-      // 背景色を指定
-      this.backgroundColor = '#444';
-      // ラベルを生成
-      this.label = Label('正解:'+ans+"円").addChildTo(this);
-      this.label.x = this.gridX.center(); // x 座標
-      this.label.y = this.gridY.center()-50; // y 座標
-      this.label.fill = 'white'; // 塗りつぶし色
-
-      var result="正解！";
-      this.label = Label(result).addChildTo(this);
-      this.label.x = this.gridX.center(); // x 座標
-      this.label.y = this.gridY.center()+50; // y 座標
-      this.label.fill = 'white'; // 塗りつぶし色
-    },
-
-    onpointstart: function() {
-      this.exit("title");//ゲームオーバーの場合
-    },
-
-  });
-
-
-  // メイン処理
-  phina.main(function() {
-
-    // アプリケーションを生成
-    var app = GameApp({
-      query: '#canvas',
-      // Scene01 から開始
-      startLabel: 'title',
-      assets: ASSETS,
-      // シーンのリストを引数で渡す
-      scenes: [
-        {
-          className: 'Title',
-          label: 'title',
-          nextLabel: 'main',
-        },
-
-        {
-          className: 'Main',
-          label: 'main',
-          nextLabel: 'answer',
-        },
-        {
-          className: 'Answer',
-          label: 'answer',
-          nextLabel: 'result',
-        },
-        {
-          className: 'Result',
-          label: 'result',
-        },
-      ]
-    });
-    // 実行
-    app.run();
-  });
+  // 実行
+  app.run();
+});
